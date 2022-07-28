90bc6aaea3d0a1b0895d5538879fe9e95efa6bc5
VanCleef <vancleefy@protonmail.com>
2022-07-29 00:22:43 +0300

    uped

--- node-mongo.js
+++ node-mongo.js
@@ -2,7 +2,7 @@ const http = require('http')
 const { MongoClient } = require("mongodb");
 
 const hostname = '127.0.0.1'
-const port = '8080'
+const port = '3000'
 
 var server = http.createServer((req, res) => {
     res.statusCode = 200
