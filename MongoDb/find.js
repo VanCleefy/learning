12f0becc59afbb4d1f865934408527a581b70664
VanCleef <vancleefy@protonmail.com>
2022-07-29 18:00:43 +0300

    asd

--- MongoDb/find.js
+++ MongoDb/find.js
@@ -4,17 +4,21 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://m001-student:m001-student@sandbox.vmxtn.mongodb.net/Sandbox?retryWrites=true&w=majority';
 
 // Database Name
-const dbName = 'sample_airbnb';
-
-// Create a new MongoClient
 const client = new MongoClient(url);
+const dbName = 'sample_airbnb';
+const db = client.db(dbName);
+const collection = db.collection('listingsAndReviews');
+// Create a new MongoClient
+
 
 // Use connect method to connect to the Server
 client.connect(function(err, client) {
   //assert.equal(null, err);
+  console.log("-----------------------------");
   console.log("Connected correctly to server");
+  console.log("-----------------------------");
 
-  const db = client.db(dbName);
+  
 
   findDocuments(db, function() {
     client.close();
@@ -23,12 +27,14 @@ client.connect(function(err, client) {
 
 const findDocuments = function(db, callback) {
     // Get the documents collection
-    const collection = db.collection('listingsAndReviews');
+    
     //
     //console.log("Listing the coins:")
     // Find some documents
-    collection.find({"name":"Ribeira Charming Duplex"}).toArray(function(err, docs) {
+    collection.find({"accommodates": {"$gt" : 6 }, "number_of_reviews": {"$eq": 50}}).toArray(function(err, docs) {
+      console.log("###########################");
       console.log("Found the following records");
+      console.log("###########################");
       console.log(docs.forEach(element => {
           
            console.log("Name:" + element.name);
